c05277ba98e1882d904551c94a1f743258a6ca24
arwihimi <arwihimi@gmail.com>
2025-12-12 22:10:44 -0400

    cambio de bsase_url
    Se realizo el cambio de la vase_url al nuevo del proyecto, aca se debe
    cambiar la url para que pueda agarra estilos dependiendo al url de
    localhost que se use

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
     // const BASE_URL="http://localhost/blog/";
     //const BASE_URL="https://proyectocgs.com/";
-    const BASE_URL="http://cursoplantitulacion.test:8080/"; //cambiar la ruta yo estoy usando laragon 
+    const BASE_URL="http://blogcursoplantitulacion.test:8080/"; //cambiar la ruta yo estoy usando laragon 
     const HOST="localhost";
     const DB="blog";
     const USER="root";
